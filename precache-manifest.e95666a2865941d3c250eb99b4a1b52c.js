7157395b2f8d05a1c209430958dbcaf46d437df8
Hanan <ensw03217@student.su.edu.krd>
2020-07-10 17:22:23 +0300

    Updates

---
+++ precache-manifest.e95666a2865941d3c250eb99b4a1b52c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f94c1b5cf3a6d10f878080981af6de",
+    "url": "/myProfile/index.html"
+  },
+  {
+    "revision": "3eea6c71a6cfccbb5bb2",
+    "url": "/myProfile/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fa61f4d16bee16e9a424",
+    "url": "/myProfile/static/css/main.e113cf57.chunk.css"
+  },
+  {
+    "revision": "3eea6c71a6cfccbb5bb2",
+    "url": "/myProfile/static/js/2.bdb8110d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myProfile/static/js/2.bdb8110d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa61f4d16bee16e9a424",
+    "url": "/myProfile/static/js/main.18f4c204.chunk.js"
+  },
+  {
+    "revision": "235b93b4d72fd1a324b8",
+    "url": "/myProfile/static/js/runtime-main.6cd75621.js"
+  },
+  {
+    "revision": "e8c71ad714d9d5db932161736f5047c5",
+    "url": "/myProfile/static/media/moviePic.e8c71ad7.jpg"
+  },
+  {
+    "revision": "067586ac8171bc8ce3a89f0145f88209",
+    "url": "/myProfile/static/media/myPic.067586ac.jpg"
+  },
+  {
+    "revision": "35bbed25de2ca8b3925363da9d23c1d0",
+    "url": "/myProfile/static/media/resturante.35bbed25.jpg"
+  },
+  {
+    "revision": "75fbfa4fadc253084ddbe92fb68aa0e3",
+    "url": "/myProfile/static/media/todo.75fbfa4f.jpg"
+  },
+  {
+    "revision": "bfeba671e0b0a7a80611d3bac4ab1ce2",
+    "url": "/myProfile/static/media/wallet.bfeba671.png"
+  }
+]);
